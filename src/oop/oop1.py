49a7186eadb74c4a80364d3ff565dbbb901cd66f
Kyle Porter <kyleporter@Kyles-Air.attlocal.net>
2020-04-10 12:37:39 -0400

    test passed for oop1 and oop2

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# base class for Vehicle
+class Vehicle:
+    pass
+
+# class for GroundVehicle
+class GroundVehicle(Vehicle):
+    pass
+
+# class for FlightVehicle
+class FlightVehicle(Vehicle):
+    pass
+
+# class for Starship
+class Starship(FlightVehicle):
+    pass
+
+# class for Airplane
+class Airplane(FlightVehicle):
+    pass
+
+# class for Car
+class Car(GroundVehicle):
+    pass
+
+# class for Motorcycle
+class Motorcycle(GroundVehicle):
+    pass
